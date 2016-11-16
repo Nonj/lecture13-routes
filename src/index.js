ffa3840ab6b421bc8bbdfc15b75ec0bdaff7a9f5
Your Name <you@example.com>
2016-11-16 09:17:17 -0800

    Route test

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom';
 import App from './PetApp';
 import {AboutPage, ResourcesPage} from './About';
 import AdoptPage from './AdoptDog';
+import {Router, Route, browserHistory} from 'react-router';
 
 //css files
 import './pet-app.css'; //load CSS for app
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <App/>,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/about" component={AboutPage} />
+    <Route path="/resources" component={ResourcesPage} />
+    <Route path="/information" component={ResourcesPage} />
+  </Router>
+  ,
   document.getElementById('root')
 );
